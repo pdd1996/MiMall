7055f560512b388e0cfe9a171ca8cca16f1c12ab
pdd1996 <pdd337816464@outlook.com>
2021-01-05 14:46:14 +0800

    feat: init router

---
+++ src/pages/order.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <order-header></order-header>
+    <router-view></router-view>
+    <order-footer></order-footer>
+  </div>
+</template>
+
+<script>
+import OrderHeader from '../components/OrderHeader.vue'
+import OrderFooter from '../components/OrderFooter.vue'
+export default {
+  components: { OrderHeader, OrderFooter },
+  name: 'order'
+}
+</script>
